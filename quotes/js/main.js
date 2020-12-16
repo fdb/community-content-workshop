a4db2730bf57b9c3a47c7ac1363eca0754213527
Frederik De Bleser <frederik@debleser.be>
2020-12-16 09:45:43 +0100

    Quotes: add new quotes.

--- quotes/js/main.js
+++ quotes/js/main.js
@@ -1,6 +1,7 @@
 const db = firebase.firestore();
 
 async function getQuotes() {
+  document.getElementById("quotes-wrapper").innerHTML = "";
   const quotesRef = db.collection("quotes");
   const docs = await quotesRef.get();
   docs.forEach((doc) => {
@@ -18,4 +19,36 @@ async function getQuotes() {
   });
 }
 
+async function handleSubmit(e) {
+  // Don't allow the form to submit (refreshing the page)
+  e.preventDefault();
+
+  // Get references to the fields.
+  const textField = document.querySelector("#text-field");
+  const authorField = document.querySelector("#author-field");
+
+  // Get their values.
+  const text = textField.value;
+  const author = authorField.value;
+  if (text.trim().length === 0 || author.trim().length === 0) {
+    alert("Please fill in both quote and author.");
+    return;
+  }
+
+  // Add the quote to the database.
+  const quotesRef = db.collection("quotes");
+  await quotesRef.add({
+    text,
+    author,
+  });
+
+  // Empty out the fields
+  textField.value = "";
+  authorField.value = "";
+
+  // Manually refresh the quotes.
+  getQuotes();
+}
+
 getQuotes();
+document.querySelector("form").addEventListener("submit", handleSubmit);
